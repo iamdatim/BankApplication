f31b216db89533a1131495974c39714d821c6b2b
Dairo Timileyin <timileyin4baze@gmail.com>
2023-05-13 19:29:40 +0100

    working on the login page

--- BankApp/LoginPage.cs
+++ BankApp/LoginPage.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BankApp_Methods;
 
 namespace BankApp
 {
     internal class LoginPage
     {
+        public static void Login(List<User> Users)
+        {
+            string loginusername = Console.ReadLine();
+        }
     }
 }
